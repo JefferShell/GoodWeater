144ed9a882fd5bbcffe2a3d153ea8618b59190c3
JefferShell <1876993774@qq.com>
2014-10-13 10:15:34 +0800

    init
    Signed-off-by: JefferShell <1876993774@qq.com>

---
+++ com/chanjet/MianWeather/GoodWather.py
@@ -0,0 +1,8 @@
+'''
+Created on 2014Äê10ÔÂ13ÈÕ
+
+@author: administrator
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ com/chanjet/MianWeather/GrabWeatherData.py
@@ -0,0 +1,54 @@
+#_*_ coding:utf-8 _*_
+'''
+Created on 2014年10月12日
+
+@author: Administrator
+'''
+import urllib2
+
+#得到省份代码  01|北京,02|上海,03|天津。。。。。
+url1 = 'http://m.weather.com.cn/data5/city.xml'
+content = urllib2.urlopen(url1).read()
+print content
+provinces = content.split(',')
+
+url = 'http://m.weather.com.cn/data3/city%s.xml'
+#得到城市代码  0501|哈尔滨,0502|齐齐哈尔,0503|牡丹江,0504|
+cities = []
+districts = []
+destination = []
+countyName = []
+for p in provinces:
+    try:
+        p_code = p.split('|')[0]
+        url2 = url % p_code
+        content2 = urllib2.urlopen(url2).read()
+#         print 'content2:',content2
+        cities +=content2.split(',')
+#         print 'len:',len(cities)
+    except:
+        print str(p_code),'失败'
+        #得到地区代码  010101|北京,010102|海淀,010103....
+# print '长度',len(cities)
+for c in cities:
+    c_code = c.split('|')[0]
+    url3 = url % c_code
+    content3 = urllib2.urlopen(url3).read()
+#     print 'content3',content3
+    districts += content3.split(',')  
+for d in districts:
+    try:
+        d_pair = d.split('|')
+        d_code = d_pair[0]
+        countyName += d_pair[1]
+        url4 = url % d_code
+        content4 = urllib2.urlopen(url4).read()
+#         print content4
+        destination += content4.split('|')[1]
+    except:
+        print d_code,'失败了'
+line = "'%s':'%s',\n" % (countyName,destination)
+file = open("d:1.txt","w+")
+file.writelines(line)
+
+# print countyName + ':' + destination
